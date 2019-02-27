831a41bd3f5d7b5e75b5ddbde5c25ddb68efa7d1
thefreer98 <thefreer@163.com>
2019-02-28 00:30:56 +0800

    修正一些参数逻辑，逻辑略微改动

--- spider/taobao/core.py
+++ spider/taobao/core.py
@@ -20,41 +20,49 @@ import random
 from pyppeteer.launcher import launch  # 控制模拟浏览器用
 from retrying import retry  #设置重试次数用的
 
-
 class Searcher():
-    def __init__(self, cookie, keyword):
+    def __init__(self, url):
         """
 		搜索，得到商品列表
-		:param cookie:
-		:param keyword:
 		"""
         ## cookie 和浏览器 UA 是绑定的
         self.headers = {
-            "Cookie": cookie,
-            "Referer": "https://s.taobao.com/search/_____tmd_____/verify/",
+            "Cookie": "",
+            # "Referer": "https://s.taobao.com/search/_____tmd_____/verify/",
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:56.0) Gecko/20100101 Firefox/56.0",
             # "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.9 Safari/537.36",
         }
-        self.url = "https://s.taobao.com/search?q=%s&sort=sale-desc" % keyword
+        self.url = url
 
-    def list_parser(self):
+    def list_parser(self, cookies):
         """
-		获取商品列表
-		:return: good_list
-		"""
-        resp = requests.get(self.url, headers=self.headers)
+        获取商品列表
+        :param cookie:
+        :return:
+        """
+        headers = self.headers
+        headers["Cookie"] = cookies
+        url = self.url
+        try:
+            resp = requests.get(url, headers=headers)
+        except:
+            return []
         html = resp.text
+        # print(html)
         page_config = re.findall("g_page_config = {(.*)}", html)
-        js = json.loads("{" + page_config[0] + "}")
-        good_list = js['mods']['itemlist']['data']['auctions']
-        return good_list
+        if len(page_config) != 0:
+            js = json.loads("{" + page_config[0] + "}")
+            good_list = js['mods']['itemlist']['data']['auctions']
+            return good_list
+        else:
+            return []
 
-    def get_data(self):
+    def get_data(self, cookies):
         """
 		返回待存入表格的数据
 		:return:
 		"""
-        good_list = self.list_parser()
+        good_list = self.list_parser(cookies=cookies)
         goods = []
         if len(good_list) >= 10:
             count = 10
@@ -165,20 +173,18 @@ class Downloader():
 
 
 class Cookier():
-    def __init__(self, cookies, url):
+    def __init__(self):
         """
 		更换cookies
-		:param cookies: 待更换的
-		:param url:
 		"""
-        self.cookies = cookies
-        self.url = url
         self.rtn_cookies = ""
         self.flag = 0
 
-    async def cookie_main(self):
+    async def cookie_main(self, url, cookies):
         """
 		定义main协程函数，初始化测试环境，主控制
+		:param url:
+		:param cookies: 待更换的
 		:return: 成功则返回新的 cookies， 否则返回 flag 1
 		"""
         # 以下使用await 可以针对耗时的操作进行挂起
@@ -190,9 +196,9 @@ class Cookier():
         await page.setUserAgent(
             'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36'
         )
-        await page.setExtraHTTPHeaders({'Cookie': self.cookies})
+        await page.setExtraHTTPHeaders({'Cookie': cookies})
         # await page.reload()
-        await page.goto(self.url)  # 访问登录页面
+        await page.goto(url)  # 访问登录页面
         # 替换淘宝在检测浏览时采集的一些参数。
         # 就是在浏览器运行的时候，始终让window.navigator.webdriver=false
         # navigator是windiw对象的一个属性，同时修改plugins，languages，navigator 且让
@@ -217,9 +223,9 @@ class Cookier():
             print('当前页面出现滑块')
             page = await self.mouse_slide(page=page)  # js拉动滑块过去。
             await page.reload()
-            self.rtn_cookies = await self.get_cookies(
-                page)  # 导出cookie 完成登陆后就可以拿着cookie玩各种各样的事情了。
-            return self.rtn_cookies
+            self.rtn_cookies = await self.get_cookies(page)  # 导出cookie 完成登陆后就可以拿着cookie玩各种各样的事情了。
+            # await page.close()
+            return self.rtn_cookies, await page.content()
         else:
             print("当前页面未出现滑块")
             self.flag = 1
@@ -229,17 +235,21 @@ class Cookier():
         """
 		make cookies
 		:param page: page 对象
+		:param old_cookies: 旧的 cookies
 		:return: 返回自己制作的 cookies
 		"""
         cookies_list = await page.cookies()
         cookies = ''
         for cookie in cookies_list:
             str_cookie = '{0}={1};'
-            str_cookie = str_cookie.format(
+            if cookie.get('name') == "x5sec":
+                str_cookie = str_cookie.format(
                 cookie.get('name'), cookie.get('value'))
-            cookies += str_cookie
-        rtn_cookies = self.cookies + "; " + cookies
-        print(cookies)
+                cookies += str_cookie
+            else:
+                pass
+        rtn_cookies = "; " + cookies
+        # print(cookies)
         return rtn_cookies
 
     def retry_if_result_none(result):
@@ -256,9 +266,10 @@ class Cookier():
         await asyncio.sleep(2)
         await page.hover('span.btn_slide')  # 不同场景的验证码模块可能名字不同。
         await page.mouse.down()
+        # await asyncio.sleep(1)
         await page.mouse.move(
             2000, 2000,
-            {'delay': random.randint(1000, 2000)})  # 不同场景的验证码滑动长度和方向可能不同同。
+            {'delay': random.randint(400000, 600000)})  # 不同场景的验证码滑动长度和方向可能不同同。
         await page.mouse.up()
         return page
 
@@ -276,12 +287,12 @@ if __name__ == "__main__":
 
     os.makedirs('./taobao', exist_ok=True)  #总文件夹
     tb_downloader = Downloader()
+    tb_searcher = Searcher()
     keyword = parse.quote("三星 液晶电视 电源板")
     with open('sec_data/cookies.txt', 'r') as c:
         cookie = c.readline()
-    for i in range(1):
-        tb_searcher = Searcher(cookie, keyword)
-        goods = tb_searcher.get_data()
+    for i in range(10):
+        goods = tb_searcher.get_data(cookie, keyword)
         print(goods)
         print(len(goods))
     # i = 0
