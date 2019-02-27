b9a9b54604b6051fad6f8ea067f33a36a1ec54fe
thefreer98 <thefreer@163.com>
2019-02-27 19:17:24 +0800

    淘宝爬虫工具模块

--- spider/taobao/modules.py
+++ spider/taobao/modules.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
 '''
-@File    :   modules.py    
+@File    :   modules.py
+@Desc    :   工具模块
 @Contact :   thefreer@outlook.com
 @License :   (C)Copyright 2018-2019, TheFreer.NET
 @WebSite :   www.thefreer.net
-@Modify Time      @Author    @Version    @Desciption
-------------      -------    --------    -----------
-2019/2/27 17:22   thefreer      1.0         工具模块
+@Modify Time      @Author    @Version
+------------      -------    --------
+2019/2/27 17:22   thefreer      1.0
 '''
 import re
 import random
