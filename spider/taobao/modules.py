d7bee3cbc093fb912ccab775d5fa8fb450782295
thefreer98 <thefreer@163.com>
2019-02-27 19:06:22 +0800

    增加淘宝爬虫

---
+++ spider/taobao/modules.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@File    :   modules.py    
+@Contact :   thefreer@outlook.com
+@License :   (C)Copyright 2018-2019, Liugroup-NLPR-CASIA
+@WebSite :   www.thefreer.net
+@Modify Time      @Author    @Version    @Desciption
+------------      -------    --------    -----------
+2019/2/27 17:22   thefreer      1.0         
+'''
