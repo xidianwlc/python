a043757518ced95df00c4cbf29f2afda693d80dd
thefreer98 <thefreer@163.com>
2019-02-27 19:12:35 +0800

    淘宝爬虫主控制

---
+++ spider/taobao/main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@File    :   main.py    
+@Contact :   thefreer@outlook.com
+@License :   (C)Copyright 2018-2019, Liugroup-NLPR-CASIA
+@WebSite :   www.thefreer.net
+@Modify Time      @Author    @Version    @Desciption
+------------      -------    --------    -----------
+2019/2/27 19:12   thefreer      1.0         
+'''
