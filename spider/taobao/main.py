86db6aab1c4342ed465ad0a31f2bc980597fb45c
thefreer98 <thefreer@163.com>
2019-03-12 23:25:10 +0800

    调整了控制逻辑

--- spider/taobao/main.py
+++ spider/taobao/main.py
@@ -11,43 +11,46 @@
 2019/2/27 19:12   thefreer      1.0
 '''
 from urllib import parse
-import asyncio
-import re
-import time
+import xlwt
 from core import Searcher, Cookier
-from modules import cookie_replace
+from modules import keywords_reader, set_table, sleepRandom
+import re
+import asyncio
 
-keyword = parse.quote("三星 液晶电视 主板")
-url = "https://s.taobao.com/search?q=%s&sort=sale-desc" % keyword
-with open('sec_data/cookies.txt', 'r') as f:
-    ori_cookies = re.sub("; x5sec=(.*)", "", f.readline())
-searchr = Searcher(url=url)
-cookier = Cookier()
-use_cookies = ori_cookies
-limit = 0
-while True:
-    limit += 1
-    if limit <= 20:
-        goods = searchr.get_data(cookies=use_cookies)
-        time.sleep(1)
-        if len(goods) == 0:
-            try:
-                loop = asyncio.get_event_loop(
-                )  # 协程，开启个无限循环的程序流程，把一些函数注册到事件循环上。当满足事件发生的时候，调用相应的协程函数。
-                new_cookies, html = loop.run_until_complete(
-                    cookier.cookie_main(url=url,
-                                        cookies=use_cookies))  # 将协程注册到事件循环，并启动事件循环
-                if re.search(r"刷新", html):
-                    print("此 Cookies 过度使用，为了可持续发展，将更换 Cookies...")
-                    cookie_replace([], ori_cookies)
-                    break
-                elif re.search(r"通过", html):
-                    loop.stop()
-                    print("验证通过...")
-                    use_cookies = ori_cookies + new_cookies
-                else:
-                    print("未知错误，停止运行...")
-            except:
-                pass
-        else:
-            pass
+if __name__ == '__main__':
+	path = "./in_data/excels/0.xls"
+	print("当前爬取的 excel 表格路径 %s" % path)
+	with open('sec_data/cookies.txt', 'r') as f:
+		cookies = f.readline()
+	f.close()
+	keywords = keywords_reader(path)
+	cookier = Cookier()
+	db, cursor = set_table()
+	index = 0
+	for key in keywords:
+		print("####=====index:%s, keyword:%s====####" % (str(index), key))
+		index+=1
+		sleepRandom(8)
+		k = "iphone"
+		keyword = parse.quote(k)
+		url = "https://s.taobao.com/search?q=%s&sort=sale-desc" % keyword
+		searcher = Searcher(url=url, db=db, cursor=cursor)
+		goods, db, cursor = searcher.get_data(cookies)
+		if goods != {}:
+			print("succ next, print goods")
+			print(goods)
+		else:
+			print("fail will try")
+			try:
+				loop = asyncio.get_event_loop()  # 协程，开启个无限循环的程序流程，把一些函数注册到事件循环上。当满足事件发生的时候，调用相应的协程函数。
+				new_cookies = loop.run_until_complete(cookier.cookie_main(url=url, cookies=cookies))  # 将协程注册到事件循环，并启动事件循环
+				cookies = re.sub("x5sec=(.*)", "", cookies)
+				cookies += new_cookies
+			except:
+				pass
+			with open('sec_data/cookies.txt', 'w') as c:
+				c.write(cookies)
+			c.close()
+			goods, _, _ = searcher.get_data(cookies)
+			print("try again, print goods")
+			print(goods)
